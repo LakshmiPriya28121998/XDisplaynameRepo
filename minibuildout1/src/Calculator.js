d43d5c58e32a5e216645b40273312664e94f8639
Lakshmi priya <soundcuttie@gmail.com>
2024-05-10 17:14:13 +0530

    calculatorcommit

--- minibuildout1/src/Calculator.js
+++ minibuildout1/src/Calculator.js
@@ -2,38 +2,61 @@ import {useState,useEffect} from 'react'
 
 export default function Displayname() {
 
-    const [input,setInput] = useState("")
+    const [input,setInput] = useState("");
+    const [res,setRes] = useState(0);
+    const[flag,setFlag] = useState(false)
 
+    const handleclick = (event) => {
+        setInput(input + event.target.id)
+    }
+    const handleClear =() => {
+        setInput("");
+        setFlag(false);
+    }
 
+    const handleequal =() => {
+        if(input == ""){
+            console.log("as")
+            setRes("Error");
+            setFlag(true);
+        }
+        else{
+            setRes(eval(input));
+            setFlag(true);
+            console.log("asas")
+        }
+            
+    }
  
     return (
         <div>
             <h1>React Calculator</h1>
             <div>
             <input type="text" value={input}></input>
-            <div>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>+</button>
+    {flag && <div>{res}</div>}
+            <div onClick={handleclick}>
+            <button id="7">7</button>
+            <button id="8">8</button>
+            <button id="9">9</button>
+            <button id="+">+</button>
             </div>
-            <div>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
+            <div onClick={handleclick}>
+            <button id="4">4</button>
+            <button id="5">5</button>
+            <button id="6">6</button>
+            <button id="-">-</button>
             </div>
-            <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>*</button>
+            <div onClick={handleclick}>
+            <button id="1">1</button>
+            <button id="2">2</button>
+            <button id="3">3</button>
+            <button id="*">*</button>
             </div>
-            <div>
-            <button>C</button>
-            <button>0</button>
-            <button>=</button>
-            <button>/</button>
+            <div >
+            <button onClick={handleClear}>C</button>
+            <button id="0" onClick={handleclick}>0</button>
+            <button onClick={handleequal}>=</button>
+            <button id="/" onClick={handleclick}>/</button>
             </div>
             </div>
         </div>
