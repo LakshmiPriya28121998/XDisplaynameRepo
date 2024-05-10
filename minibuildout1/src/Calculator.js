4767290a39f58c957fbeb3a28f6f94668fc4497f
Lakshmi priya <soundcuttie@gmail.com>
2024-05-10 16:38:41 +0530

    calculatordisplay

--- minibuildout1/src/Calculator.js
+++ minibuildout1/src/Calculator.js
@@ -9,23 +9,33 @@ export default function Displayname() {
     return (
         <div>
             <h1>React Calculator</h1>
+            <div>
             <input type="text" value={input}></input>
+            <div>
             <button>7</button>
             <button>8</button>
             <button>9</button>
             <button>+</button>
+            </div>
+            <div>
             <button>4</button>
             <button>5</button>
             <button>6</button>
             <button>-</button>
+            </div>
+            <div>
             <button>1</button>
             <button>2</button>
             <button>3</button>
             <button>*</button>
+            </div>
+            <div>
             <button>C</button>
             <button>0</button>
             <button>=</button>
             <button>/</button>
+            </div>
+            </div>
         </div>
     )
 }
